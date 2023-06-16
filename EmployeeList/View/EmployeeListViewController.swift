2a6d4de9280a4d1de42c662d972f742caee91220
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-06-16 19:23:01 +0300

    Created EmployeeListViewController

---
+++ EmployeeList/View/EmployeeListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EmployeeListViewController.swift
+//  EmployeeList
+//
+//  Created by Vlad on 16.06.2023.
+//
+
+import UIKit
+
+class EmployeeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
+
+extension EmployeeListViewController: EmployeeListViewProtocol {
+    func reloadData() {
+        
+    }
+    
+    func showError() {
+        
+    }
+    
+    
+}
