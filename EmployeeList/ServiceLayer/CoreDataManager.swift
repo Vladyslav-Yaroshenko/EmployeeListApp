d839127962dda7b896b2cf469e1b05a5989fd8bf
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-06-16 16:18:14 +0300

    Created entity employee for core data, create CoreDataManager class

---
+++ EmployeeList/ServiceLayer/CoreDataManager.swift
@@ -0,0 +1,68 @@
+//
+//  CoreDataManager.swift
+//  EmployeeList
+//
+//  Created by Vlad on 16.06.2023.
+//
+
+import CoreData
+import UIKit
+
+protocol CoreDataManagingProtocol {
+    func addEmployee(name: String,
+                     lastName: String,
+                     salary: Float,
+                     birthday: Date,
+                     gender: String,
+                     department: String)
+    
+    func fetchEmployees() -> [Employee]?
+}
+
+class CoreDataManager: CoreDataManagingProtocol {
+    
+    public static let shared = CoreDataManager()
+    private init() {}
+    
+    private var appDelegate: AppDelegate {
+        UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    private var context: NSManagedObjectContext {
+        appDelegate.persistentContainer.viewContext
+    }
+    
+    private let entityName = "Employee"
+    
+    // Add new employee to core data
+    public func addEmployee(name: String,
+                            lastName: String,
+                            salary: Float,
+                            birthday: Date,
+                            gender: String,
+                            department: String) {
+        guard let employeeEntityDescription = NSEntityDescription.entity(forEntityName: entityName,
+                                                                         in: context)
+        else { return }
+        let employee = Employee(entity: employeeEntityDescription,
+                                insertInto: context)
+        employee.name = name
+        employee.lastName = lastName
+        employee.salary = salary
+        employee.birthday = birthday
+        employee.gender = gender
+        employee.department = department
+        
+        appDelegate.saveContext()
+    }
+    
+    // Fetch all employees from core data
+    func fetchEmployees() -> [Employee]? {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+        do {
+            return (try? context.fetch(fetchRequest) as? [Employee]) ?? []
+        }
+    }
+    
+    
+}
