5987ab04b56977b07cb9919f8e09d3ad408b62f0
Vladyslav-Yaroshenko <vlad.yaroshenko7@gmail.com>
2023-06-17 16:06:01 +0300

    Created AddEmpoyeeVC

---
+++ EmployeeList/AddEmployeeModule/View/AddEmployeeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmployeeViewController.swift
+//  EmployeeList
+//
+//  Created by Vlad on 17.06.2023.
+//
+
+import UIKit
+
+class AddEmployeeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
